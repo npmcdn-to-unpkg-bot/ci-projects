f91f0b981309c1916d79331a4936939ec0b34caf
User <User@192.168.1.112>
2015-10-29 23:29:42 +0300

    devam

--- application/controllers/backend/Categories.php
+++ application/controllers/backend/Categories.php
@@ -15,11 +15,9 @@ class Categories extends Backend_Controller
 
 		$this->load->model('categories_model');
 		$cat_id = $this->input->get('category');
-		var_dump($cat_id);
-		echo "<br>";
-		$get_categories_list = $this->categories_model->get_categories_list($cat_id);
-		var_dump($get_categories_list);
-
+		$cat = $this->categories_model->get_categories_list($cat_id);
+		$this->createTreeView($cat, 0);
+		// var_dump($cat);
 		$this->load->view('backend/layout/header');
 		$this->load->view('backend/categories');
 		$this->load->view('backend/layout/footer');
@@ -30,6 +28,33 @@ class Categories extends Backend_Controller
 
 		var_dump($this->input->post());
 		exit;
+	}
+
+	function createTreeView($array, $currentParent, $currLevel = 0, $prevLevel = -1) {
+
+		foreach ($array as $categoryId => $category) {
+
+			if ($currentParent == $category->parent_id) {
+				echo "önce id:".$category->id;
+				echo ", cur:".$currLevel;
+				echo ", prev:".$prevLevel;
+				echo "<br/>";
+				// if ($currLevel > $prevLevel) echo "<ul class='tree'>";
+
+				// if ($currLevel == $prevLevel) echo "</li>";
+
+				// echo '<li> <label for="subfolder2">'.$category->name.', katid :'.$category->id.'</label>';
+
+				if ($currLevel > $prevLevel) { $prevLevel = $currLevel; }
+
+				$currLevel++;
+
+				$this->createTreeView($array, $category->id, $currLevel, $prevLevel);
+
+				$currLevel--;
+			}
+		}
+		// if ($currLevel == $prevLevel) echo "</li></ul> ";
 	}
 }
 ?>
--- application/models/Categories_Model.php
+++ application/models/Categories_Model.php
@@ -5,18 +5,29 @@
 class Categories_Model extends CI_Model
 {
 	
-	function get_categories_list($cat_id) {
+	function get_categories_list($cat_id =  null) {
 
-		if ($cat_id <= 0 || $cat_id == null) {
-			$cat_id = "node.parent_id = '0'";
-			$query = $this->db->query('SELECT node.name ,node.cat_link,node.id, node.description, (COUNT( parent.name ) - ( sub_tree.depth +1 )) AS depth FROM categories AS node, categories AS parent, categories AS sub_parent, (SELECT node.id, (COUNT( parent.id ) -1) AS depth FROM categories AS node, categories AS parent WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND '.$cat_id.' GROUP BY node.id ORDER BY node.left_node) AS sub_tree WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND node.left_node BETWEEN sub_parent.left_node AND sub_parent.right_node AND sub_parent.id = sub_tree.id GROUP BY node.id HAVING depth < 1 ORDER BY node.left_node');
-		} else {
-			$cat_id = "node.id = '".$cat_id."'";
-			$query = $this->db->query('SELECT node.name ,node.cat_link,node.id, node.description, (COUNT( parent.name ) - ( sub_tree.depth +1 )) AS depth FROM categories AS node, categories AS parent, categories AS sub_parent, (SELECT node.id, (COUNT( parent.id ) -1) AS depth FROM categories AS node, categories AS parent WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND '.$cat_id.' GROUP BY node.id ORDER BY node.left_node) AS sub_tree WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND node.left_node BETWEEN sub_parent.left_node AND sub_parent.right_node AND sub_parent.id = sub_tree.id GROUP BY node.id HAVING depth = 1 ORDER BY node.left_node');
+		$this->db->select('id, parent_id, status, cat_link, name, queue');
+		$query = $this->db->get('categories');
+		if ($cat_id !== null) {
+			$this->db->where('id', $cat_id);
+			$query = $this->db->get('categories');
 		}
-		// $query_string = 'SELECT node.name ,node.cat_link,node.id, node.description, (COUNT( parent.name ) - ( sub_tree.depth +1 )) AS depth FROM categories AS node, categories AS parent, categories AS sub_parent, (SELECT node.id, (COUNT( parent.id ) -1) AS depth FROM categories AS node, categories AS parent WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND '.$cat_id.' GROUP BY node.id ORDER BY node.left_node) AS sub_tree WHERE node.left_node BETWEEN parent.left_node AND parent.right_node AND node.left_node BETWEEN sub_parent.left_node AND sub_parent.right_node AND sub_parent.id = sub_tree.id GROUP BY node.id HAVING depth = 1 ORDER BY node.left_node';
-		// var_dump($query_string);
-		// exit;
+		
+		if ($query->num_rows()>0) {
+
+			return $result = $query->result();
+		}
+	}
+
+	function get_categories($cat_id =  null) {
+
+		$query = $this->db->get('categories');
+		if ($cat_id !== null) {
+			$this->db->where('id', $cat_id);
+			$query = $this->db->get('categories');
+		}
+		
 		if ($query->num_rows()>0) {
 
 			return $result = $query->result();
