ceeba869ae4a690d49300d4446c098a1a1816ac2
huseyindol <huseyindol@gmail.com>
2016-01-12 15:37:02 +0200

    is 12ocak. file_management folder ekle-sil tamamland, file baslancak.

--- application/controllers/backend/Files_Management.php
+++ application/controllers/backend/Files_Management.php
@@ -29,21 +29,28 @@ class Files_Management extends Backend_Controller
 						$data['folders'][$key] = (object)$value;
 					}
 				}
-				$relative_path = $value['relative_path'];
-				$data['relative_path'] = $value['relative_path'];
+				// $relative_path = $value['relative_path'];
+				// $data['relative_path'] = $value['relative_path'];
+				$relative_path = str_replace("\\", "/", $value['relative_path']);
+				$data['relative_path'] = str_replace("\\", "/", $value['relative_path']);
 			}
 		} else {
-			$relative_path = $get_dir;
-			$data['relative_path'] = $get_dir;
+			// $relative_path = $get_dir;
+			// $data['relative_path'] = $get_dir;
+			$relative_path = str_replace("\\", "/", $get_dir);
+			$data['relative_path'] = str_replace("\\", "/", $get_dir);
 		}
-		$relative_path = explode(FCPATH, $relative_path);
+		$fcpath = str_replace("\\", "/", FCPATH);
+		// $relative_path = explode(FCPATH, $relative_path);
+		$relative_path = explode($fcpath, $relative_path);
 		$relative_path = explode('/', $relative_path[1]);
 		$breadcrumb = '';
 		foreach($relative_path as $l){
 			$breadcrumb .= $l;
 			if ($l != 'assets') {
 				$data['breadcrumb'][$l] = (object)array(
-					'crumb' => FCPATH.$breadcrumb,
+					// 'crumb' => FCPATH.$breadcrumb,
+					'crumb' => $fcpath.$breadcrumb,
 					'bread' => $l
 				);
 			}
@@ -60,17 +67,17 @@ class Files_Management extends Backend_Controller
 		$make_dir = $this->input->post('relative_path').'/'.$this->input->post('folder');
 		if(file_exists($make_dir)) {
 			$this->session->set_flashdata('errors',$this->input->post('folder').' adlı klasör vardır.');
-			redirect('backend/files_management');
+			redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 		} else {
 			if ($make_dir) {
 				$old_umask = umask(0);
 				$make_dir = mkdir($this->input->post('relative_path').'/'.$this->input->post('folder'),0777);
 				umask($old_umask);
 				$this->session->set_flashdata('success','klasör eklendi.');
-				redirect('backend/files_management');
+				redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 			} else {
 				$this->session->set_flashdata('errors','klasör eklenemedi.');
-				redirect('backend/files_management');
+				redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 			}
 		}
 	}
@@ -79,15 +86,15 @@ class Files_Management extends Backend_Controller
 		$delete_dir = $this->input->get('dir');
 		if(!file_exists($delete_dir)) {
 			$this->session->set_flashdata('errors',$this->input->post('folder').' adlı klasör yoktur.');
-			redirect('backend/files_management');
+			redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 		} else {
 			if ($delete_dir) {
 				rmdir($this->input->get('dir'));
 				$this->session->set_flashdata('success','klasör silindi.');
-				redirect('backend/files_management');
+				redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 			} else {
 				$this->session->set_flashdata('errors','klasör silinemedi.');
-				redirect('backend/files_management');
+				redirect('backend/files_management?dir='.$this->input->post('relative_path'));
 			}
 		}
 	}
